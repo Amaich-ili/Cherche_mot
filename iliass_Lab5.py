a1bab15f8cb0e96298254b027dc1426c07be8d81
Iliass <iliamaich@gmail.com>
2022-10-17 17:42:20 -0400

    Update iliass_Lab5.py

--- iliass_Lab5.py
+++ iliass_Lab5.py
@@ -26,25 +26,29 @@ def list_mot():
             sortir = True
         else:
             list_mot.append(mot)
-
     return list_mot
 
-def recherche_recur():
-    list = list_mot()
-    index = randint(0, len(list)-1)
-    mot_index = list[index]
-    print(mot_index)
-    print(list)
-    input1 = input("mot : ")
-    compte = 0
-    if mot_index == input1:
-        print(compte) 
-
+def recherche_recur(inpute, ma_liste_mot):
+    index = randint(0, len(ma_liste_mot)-1)
+    mot_index = ma_liste_mot[index]
+    if mot_index != inpute:
+        return 1+ recherche_recur(inpute, ma_liste_mot)
     else :
-        list.pop(index)
-        print(list)
+        return 0
+    # return mot_index
 
-recherche_recur()
+
+# def recursif(inpute):
+#     liste =list_mot()
+#     mot =  recherche_recur(liste)
+#     if mot != inpute:
+#         return 1+ recursif(inpute)
+#     else :
+#         return 0
+
+
+
+# recherche_recur()
 
 
 def recherche_mot():
@@ -60,18 +64,42 @@ def recherche_mot():
             print("Le mot saisi n'est pas dans la liste !!")
 
         else:
-            recherche_recur(ma_liste_mot)
+            print(recherche_recur(inpute,ma_liste_mot))
             sortir = True
-    return inpute
-
-# recherche_mot()
-
-
     
 
+recherche_mot()
 
 
-            
+# sortir = False
+# compte = 0
+# while not sortir:
+#     index = randint(0, len(ma_liste_mot)-1)
+#     mot_index = ma_liste_mot[index]
+    
+#     print(mot_index)
+#     try:
+#         if mot_index != inpute:
+#             ma_liste_mot.pop(index)
+#             compte+=1
+#     except:
+#         pass
+
+#     else :
+#         print(compte)
+#         sortir = True 
+
+
+# list =["a","b","v","h","","5","6","7","8","9"]
+# def myst(l: list) -> int:
+#     if l==[]:
+#         return 0
+#     else:          
+#         l.pop(0)          # suppression du premier terme de la liste l
+#         return 1+myst(l)
+
+# print(myst(list))
+        
 
 
 
