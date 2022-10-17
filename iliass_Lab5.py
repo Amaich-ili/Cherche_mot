b631832833ea029f59cdb1a5f71df40fecb8a01a
Iliass <iliamaich@gmail.com>
2022-10-17 16:30:02 -0400

    Update iliass_Lab5.py

--- iliass_Lab5.py
+++ iliass_Lab5.py
@@ -4,6 +4,9 @@ Laboratoire 5
 Session été 2022
 
 """
+from msvcrt import kbhit
+from random import randint
+from re import T
 def verifier_mot(mot):
     list_number = ["0","1","2","3","4","5","6","7","8","9"]
     for lettre in mot:
@@ -19,16 +22,30 @@ def list_mot():
             mot = input("Saisir un mot : ")
             if verifier_mot(mot):
                 raise Exception
-
         except:
             sortir = True
-
         else:
             list_mot.append(mot)
-    print(list_mot)
-    
+
     return list_mot
-# list_mot()
+
+def recherche_recur():
+    list = list_mot()
+    index = randint(0, len(list)-1)
+    mot_index = list[index]
+    print(mot_index)
+    print(list)
+    input1 = input("mot : ")
+    compte = 0
+    if mot_index == input1:
+        print(compte) 
+
+    else :
+        list.pop(index)
+        print(list)
+
+recherche_recur()
+
 
 def recherche_mot():
     ma_liste_mot = list_mot()
@@ -43,12 +60,14 @@ def recherche_mot():
             print("Le mot saisi n'est pas dans la liste !!")
 
         else:
-            # je doit ajouter le comteur ici
+            recherche_recur(ma_liste_mot)
             sortir = True
-    
-    
     return inpute
-recherche_mot()
+
+# recherche_mot()
+
+
+    
 
 
 
