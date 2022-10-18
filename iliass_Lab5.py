82058f2e1e61ccffc64a48f5a460165485b0ec51
Iliass <iliamaich@gmail.com>
2022-10-18 14:42:07 -0400

    Update iliass_Lab5.py

--- iliass_Lab5.py
+++ iliass_Lab5.py
@@ -8,9 +8,8 @@ from msvcrt import kbhit
 from random import randint
 from re import T
 def verifier_mot(mot):
-    list_number = ["0","1","2","3","4","5","6","7","8","9"]
     for lettre in mot:
-        if lettre in list_number:
+        if lettre.isdigit():
             return True
 
 def list_mot():
@@ -33,7 +32,7 @@ def recherche_mot(inpute, ma_liste_mot):
     if mot_index != inpute:
         return 1+ recherche_mot(inpute, ma_liste_mot)
     else :
-        return 1
+        return 0
 
 def compte_recursion():
     ma_liste_mot = list_mot()
